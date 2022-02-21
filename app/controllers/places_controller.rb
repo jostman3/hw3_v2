9a25457b6fa04bced19d628f3715871f114da6d0
jostman3 <joshua.ostman@kellogg.northwestern.edu>
2022-02-21 03:51:51 +0000

    Almost done

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,26 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+
+    def new 
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+
+    def destroy 
+        @place = Place.find(params["id"])
+        @place.destroy
+        redirect_to "/places"
+    end
+end
